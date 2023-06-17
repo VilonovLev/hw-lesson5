faebbc569cd1cde8f409d6e819af5e45a9b0a728
vilonovlev@gmail.com <vilonovlev@gmail.com>
2023-06-17 12:34:25 +0300

    add comments and move metods

--- src/Main.java
+++ src/Main.java
@@ -16,57 +16,12 @@ public class Main {
         Tree.print(Paths.get("."));
 
         // Задание 3
-        int[] array = {0,2,3 ,1,2,3, 0,2,1};
+        int[] array = {3,0,1 ,1,2,3, 0,2,1};
         Path path = Paths.get(".\\text.bin");
         serializeArr(array,path);
 
         int[] array2 = deserializeArr(path);
         System.out.println(Arrays.toString(array2));
-    }
-
-    /**
-     * Сериализации массива с последующей записью в файл
-     * @param array массив для сериализации
-     * @param path путь до файла
-     * @throws IOException
-     */
-    private static void serializeArr(int[] array, Path path) throws IOException {
-        // Создаем файл, если его нет
-        if (!Files.exists(path)) {
-            Files.createFile(path);
-        }
-        try (OutputStream outputStream = Files.newOutputStream(path)) {
-            for (int i = 0; i < array.length; i += 3) {
-                // "Склеиваем" число из трех элементов массива и пишем в файл
-                int value = (array[i+2] << 4) | (array[i+1] << 2) | (array[i]) ;
-                outputStream.write(value);
-            }
-        }
-    }
-
-    /**
-     * Десериализации массива
-     * @param path путь до файла
-     * @return int[]
-     * @throws IOException
-     */
-    private static int[] deserializeArr(Path path) throws IOException {
-        if (!Files.exists(path)) {
-            throw new IllegalArgumentException();
-        }
-
-        try (InputStream inputStream = Files.newInputStream(path)) {
-            byte[] input = inputStream.readAllBytes();
-            int[] result = new int[input.length*3];
-            int x = input[0];
-            for (int i = 0, j = 0 ; i < result.length; i++ ) {
-                if (i % 3 == 0) x = input[j++];
-                // "восстанавливаем" элементы массива
-                result[i] = x & 3;
-                x = x >> 2;
-            }
-            return result;
-        }
     }
 
     /** Задание 1
@@ -93,6 +48,52 @@ public class Main {
                 Path backupDir = Paths.get(".\\backup\\" + filePath.toString());
                 Files.copy(filePath,backupDir,StandardCopyOption.REPLACE_EXISTING);
             }
+        }
+    }
+
+    /** Задание 3
+     * Сериализации массива с последующей записью в файл
+     * @param array массив для сериализации
+     * @param path путь до файла
+     * @throws IOException
+     */
+    private static void serializeArr(int[] array, Path path) throws IOException {
+        // Создаем файл, если его нет
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        try (OutputStream outputStream = Files.newOutputStream(path)) {
+            for (int i = 0; i < array.length; i += 3) {
+                // "Склеиваем" число из трех элементов массива и пишем в файл
+                int value = (array[i+2] << 4) | (array[i+1] << 2) | (array[i]) ;
+                outputStream.write(value);
+                System.out.println(value);
+            }
+        }
+    }
+
+    /** Задание 3 (Доп)
+     * Десериализации массива
+     * @param path путь до файла
+     * @return int[]
+     * @throws IOException
+     */
+    private static int[] deserializeArr(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException();
+        }
+
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            byte[] input = inputStream.readAllBytes();
+            int[] result = new int[input.length*3];
+            int x = input[0];
+            for (int i = 0, j = 0 ; i < result.length; i++ ) {
+                if (i % 3 == 0) x = input[j++];
+                // "восстанавливаем" элементы массива
+                result[i] = x & 3;
+                x = x >> 2;
+            }
+            return result;
         }
     }
 }
